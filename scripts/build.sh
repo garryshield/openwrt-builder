9e583659d203fd8a49703c106300c8e819972340
garryshield <749291704@qq.com>
2025-07-14 19:07:09 +0800

    init

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,8 +14,10 @@ pushd ./packages
 curl -sSL -O https://github.com/vernesong/OpenClash/releases/download/v0.46.120/luci-app-openclash_0.46.120_all.ipk
 popd
 
-make image \
-  PROFILE="${PROFILE}" \
-  BIN_DIR="/openwrt/bin" \
-  FILES="/openwrt/files" \
-  PACKAGES="${PACKAGES}"
+ls -la ./packages
+
+# make image \
+#   PROFILE="${PROFILE}" \
+#   BIN_DIR="/openwrt/bin" \
+#   FILES="/openwrt/files" \
+#   PACKAGES="${PACKAGES}"
