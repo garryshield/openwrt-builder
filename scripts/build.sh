ba10ef64dff41bf9b4bda63c1b750dbf8f167226
garryshield <749291704@qq.com>
2025-07-14 17:54:33 +0800

    init

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
-
+# =============  ==============
 echo '========'
 pwd
 
+# =============  ==============
 echo '========'
 env
+
+# =============  ==============
+echo '========'
+make image \
+  PROFILE="${PROFILE}" \
+  BIN_DIR="/openwrt/bin" \
+  FILES="/openwrt/files" \
+  PACKAGES="${PACKAGES}"
